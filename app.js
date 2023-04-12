0388393d7c9b218c2278c5ba63a085313221a2a9
Chen GG <123@gmail.com>
2023-04-12 23:48:49 +0800

    增加gsap的動畫

--- app.js
+++ app.js
@@ -106,3 +106,22 @@ let showothercity = document.querySelector(".otherweather");
 otherweather.addEventListener("click", () => {
   showothercity.classList.toggle("active");
 });
+let hero = document.querySelector(".hero");
+let slider = document.querySelector(".slider");
+let animation = document.querySelector("section.animation-wrapper");
+
+const time_line = new TimelineMax();
+// 要控制的對象
+// duration
+// 控制對象的初始狀態
+// 控制對象結束動畫後的狀態
+time_line
+    .fromTo(hero, 1, { height: "0%" }, { height: "100%", ease: Power2.easeInOut })
+    .fromTo(hero, 1.2, { width: "80%" }, { width: "100%", ease: Power2.easeInOut })
+    .fromTo(slider, 1, { x: "-100%" }, { x: "0%", ease: Power2.easeInOut }, "-=1.2")
+    .fromTo(animation, 0.3, { opacity: "1" }, { opacity: "0", ease: Power2.easeInOut })
+    ;
+// 在完成動畫後 讓使控制項目無法被點擊
+setTimeout(() => {
+    animation.style.pointerEvents = "none";
+}, 2500);
